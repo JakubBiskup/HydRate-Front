a8d57cf8cb09a57857947a208ee45806a8c853ad
Jakub Biskup <biskup.jakub@gmail.com>
2021-01-03 12:59:29 +0100

    Add deleting review functionality

--- public/review.js
+++ public/review.js
@@ -9,23 +9,39 @@ function getParameterByName(name, url = window.location.href) {
 let reviewId=getParameterByName("id")
 
 $(document).ready(function() {
-    $.ajax({
-        url: "http://localhost:8080/reviews/"+reviewId,
-        type : "GET",
-        success : function(data) {
-            $('.review-header').append("Review of:  " + data.water.name);
-            $('.score').append(data.score+"/100");
-            $('.review-text').append(data.text);
-            var a= document.getElementsByClassName("waterLink");
-            a.href="/water?id="+data.water.id;
+    document.getElementById('delete-button').onclick=function(e){
+                $.ajax({
+                    type: "DELETE",
+                    url: "http://localhost:8080/reviews/"+reviewId,
+                    success: function (){
+                        alert("Review of id: " +reviewId+" is deleted");
+                        window.location.href="all-reviews.html";
+                        
+                    },
+                    error: function (errMsg) {
+                        alert(errMsg);
+                    }
+                })
+            }
+        $.ajax({
+            url: "http://localhost:8080/reviews/"+reviewId,
+            type : "GET",
+            success : function(data) {
+                $('.review-header').append("Review of:  " + data.water.name);
+                $('.score').append(data.score+"/100");
+                $('.review-text').append(data.text);
+                var a= document.getElementsByClassName("waterLink");
+                a.href="/water?id="+data.water.id;
 
-            var seeWaterLink=document.createElement("a");
-            seeWaterLink.href="water.html?id="+data.water.id;
+                var seeWaterLink=document.createElement("a");
+                seeWaterLink.href="water.html?id="+data.water.id;
 
-            var seeWaterText=document.createTextNode("See Water");
-            seeWaterLink.appendChild(seeWaterText);
+                var seeWaterText=document.createTextNode("See Water");
+                seeWaterLink.appendChild(seeWaterText);
 
-            $('.see-water-link').append(seeWaterLink);
-            $('.title').append(data.id);
-            document.getElementById('edit-link').href="edit-review-form.html?review-id="+reviewId+"&name="+data.water.name;
+                $('.see-water-link').append(seeWaterLink);
+                $('.title').append(data.id);
+                document.getElementById('edit-link').href="edit-review-form.html?review-id="+reviewId+"&name="+data.water.name;
+                
             }})});
+    
