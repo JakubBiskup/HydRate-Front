62c46b2f9a4c3567be412bf6e611ebf21e476275
Jakub Biskup <biskup.jakub@gmail.com>
2021-01-08 20:01:20 +0100

    Add logout.html

---
+++ public/logout.js
@@ -0,0 +1,7 @@
+$(document).ready(function (){
+    const logoutForm=document.getElementById("logout-form");
+    logoutForm.onsubmit=function (e){
+        e.preventDefault();
+        document.cookie = "HydRateJWT=; expires=Thu, 01 Jan 1970 00:00:02 UTC; path=/C:/Users/User/Desktop/Spring/HydRate/hydrate%20front/public";
+        alert("Logout");
+    }})
