052301c1a744b9df2b257409a17d8589b8e68f4e
Jakub Biskup <biskup.jakub@gmail.com>
2021-01-09 20:50:52 +0100

    Add login/logout link to html pages

--- public/accountInfo.js
+++ public/accountInfo.js
@@ -2,8 +2,12 @@ $(document).ready(function(){
     var loggedInUserInfo=document.createElement("div");
     loggedInUserInfo.style.cssText = 'background:teal';
     var paragraph=document.createElement("p");
-    var textInP=document.createTextNode("You are authenticated as: " + recognize()+",your role: "+ recognizeRole());
+    var textInP=document.createTextNode("You are authenticated as: " + recognize());
+    var textInPLine2=document.createTextNode('Your role: '+ recognizeRole());
     paragraph.appendChild(textInP);
+    var br=document.createElement("br");
+    paragraph.appendChild(br);
+    paragraph.appendChild(textInPLine2);
     loggedInUserInfo.appendChild(paragraph);
     if(isCookieThere()){
         var logoutLink=document.createElement("a");
